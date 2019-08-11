3fad17d882a4e457db0f1df66958bf25a828b8c2
Ruslan Safin <rusel91@idz.ru>
2019-08-11 18:00:55 +0400

    fix json

--- app/Http/Resources/Photo.php
+++ app/Http/Resources/Photo.php
@@ -23,7 +23,9 @@ class Photo extends JsonResource
             'description' => $this->description,
             'active' => $this->active,
             'photo_category' => $this->photoCategory->pluck('id'),
-            'photo_tags' => $this->photoTag->pluck('name')
+            'photo_tags' => $this->photoTag->pluck('name'),
+            'image_path' => $this->image_path,
+            'image_preview_path' => $this->image_preview_path,
         ];
     }
 }
